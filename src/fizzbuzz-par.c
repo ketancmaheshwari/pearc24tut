b77817905967cd249a741baf69ccde1b49eea1fb
Ketan Maheshwari <km0@ornl.gov>
2024-07-12 10:31:21 -0400

    adding fizzbuzz

---
+++ src/fizzbuzz-par.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int i = atoi(argv[1]);
+    if (i % 3 == 0)
+        printf("Fizz");
+    if (i % 5 == 0)
+        printf("Buzz");
+    if ((i % 3 != 0) && (i % 5 != 0))
+        printf(" %d ", i);
+
+    printf("\n");
+
+    return 0;
+}
+
