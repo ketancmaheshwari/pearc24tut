299076b93c7d16c4ad923d70a72963622e277ec3
Ketan Maheshwari <km0@ornl.gov>
2024-07-17 16:10:55 -0400

    updates to slides and workflow

--- src/workflow/getdata.sh
+++ src/workflow/getdata.sh
@@ -2,17 +2,17 @@
 set -eu
 mkdir -p data
 function cleanup() {
-  \rm -f ./data/earth*.jpg
+  \rm -f ./data/*.jpg
+  \rm -f q.proc
 }
 
 while true
 do
-  ts=$(date +%s)
-  curl -s https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/se/GEOCOLOR/1200x1200.jpg --output ./data/earth${ts}.jpg
-  sleep 15
+  ts=$(date +%s) # timestamp
+  # download image data from 8 regions in parallel
+  parallel -j8 "curl -s https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/{}/GEOCOLOR/1200x1200.jpg --output ./data/{}_${ts}.jpg" ::: cgl ne nr se sp sr pr pnw
+  echo $ts >> q.proc
+  sleep 60
   trap cleanup EXIT
 done
-
-# Other regions
-# for i in cgl ne nr se sp sr pr pnw; do curl -s https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/$i/GEOCOLOR/1200x1200.jpg --output ./data/$i.jpg; done
 
--- src/workflow/procdata.sh
+++ src/workflow/procdata.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -eu
 
-TASK=convert
-DATA=$1
-echo "\nProcessing ${DATA}\n"
-${TASK} ${DATA} -fuzz 10% -fill white -opaque white -fill black +opaque white -format "%[fx:100*mean]" info:
-sleep 11
+touch q.proc ; tail -n+0 -f q.proc | parallel -k -j8 'printf "\nTimestamp:{}\n"; convert ./data/*_{}.jpg -fuzz 10% -fill white -opaque white -fill black +opaque white -format "%[fx:100*mean] " info:'
 
