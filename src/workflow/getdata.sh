a999d93f54620a6e6658241f0b0f6ba71e7183b7
Ketan Maheshwari <km0@ornl.gov>
2024-07-18 18:41:08 -0400

    more updates

--- src/workflow/getdata.sh
+++ src/workflow/getdata.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 set -eu
 mkdir -p data
+
 function cleanup() {
   \rm -f ./data/*.jpg
-  \rm -f q.proc
 }
 
 while true
@@ -11,8 +12,8 @@ do
   ts=$(date +%s) # timestamp
   # download image data from 8 regions in parallel
   parallel -j8 "curl -s https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/{}/GEOCOLOR/1200x1200.jpg --output ./data/{}_${ts}.jpg" ::: cgl ne nr se sp sr pr pnw
-  echo $ts >> q.proc
-  sleep 60
+  #<<add appropriate queueing line here to put the timestamp in queue>>
+  sleep 30
   trap cleanup EXIT
 done
 
--- src/workflow/procdata.sh
+++ src/workflow/procdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-touch q.proc ; tail -n+0 -f q.proc | parallel -k -j8 'printf "\nTimestamp:{}\n"; convert ./data/*_{}.jpg -fuzz 10% -fill white -opaque white -fill black +opaque white -format "%[fx:100*mean] " info:'
+<<something missing here>> | parallel -k -j8 'printf "\nTimestamp:{}\n"; convert ./data/*_{}.jpg -fuzz 10% -fill white -opaque white -fill black +opaque white -format "%[fx:100*mean] " info:'
 
